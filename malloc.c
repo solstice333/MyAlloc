8bdd311f92d1fc99f18f81884b20fdcc861c5e40
knavero <knavero@gmail.com>
2014-04-08 16:19:37 -0700

    Fixed bug in carveHeader dealing with adding in the size offset

--- malloc.c
+++ malloc.c
@@ -4,15 +4,27 @@ static Header *freelist = NULL;
 
 static char buffer[BUFSIZE];
 
-// Convenience function for creating a new header
-static Header *createNewHeader(int size, Header *curr, Header *next) {
-   Header *newHeader = curr + sizeof(Header) + size;
+/*
+ * Used when a smaller block of space (let's call this block1) can be carved 
+ * out from a bigger one for allocation purposes. block1 then points to 
+ * the remaining excess (let's call this block2). block2 is returned by
+ * carveHeader(). |size| represents the size of block2, |curr| represents
+ * the pointer to the start of the initial big block, |next| represents
+ * the Header pointer of the initial big block which will be redirected as 
+ * block1 pointing to block2.
+ */
+static Header *carveHeader(int size, Header *curr, Header *next) {
+   Header *newHeader = (Header *)((char *)curr + sizeof(Header) + 
+    curr->size - size);
    newHeader->free = 1;
    newHeader->size = size;
    newHeader->next = next;
    return newHeader;
 }
 
+/*
+ *  Pushes |add| to the back of the freelist
+ */
 static void push_back(Header *add) {
    Header *h = freelist, *prev = NULL;
    
@@ -29,38 +41,62 @@ static void push_back(Header *add) {
    }
 }
 
+/*
+ * Merges contiguous free blocks in the freelist
+ */
+static void defrag() {
+   Header *curr = freelist, *prev = NULL, *first = NULL;
+   int sum = 0;
+
+   while (curr) {
+      if (curr->free) {
+         if (!prev || !prev->free) 
+            first = curr; 
+
+         sum += curr->size;
+         if (!curr->next || !curr->next->free) {
+            first->size = sum;
+            sum = 0;
+            first->next = curr->next;
+         }
+      }
+
+      prev = curr;
+      curr = curr->next;
+   }
+}
+
+/*
+ * Traverses the freelist and prints out header information
+ */
 void checkFreelist() {
    Header *h = freelist;
    while (h) {
-      snprintf(buffer, BUFSIZE, "free: %d, size: %d, next: %d",
-       h->free, h->size, h->next);
+      snprintf(buffer, BUFSIZE, "at: %d, free: %d, size: %d, next: %d",
+       h, h->free, h->size, h->next);
       puts(buffer);
       h = h->next;
    }
+   snprintf(buffer, BUFSIZE, "current section break: %d", sbrk(0));
+   puts(buffer);
 }
 
 // TODO implement this function first
 void *myMalloc(size_t size) {
-   Header *h = freelist;
+   Header *curr = freelist, *prev = NULL, *first = NULL;
 
-   int sum = 0;
-   while (h) {
-      if (h->size > size) {
-         h->next = createNewHeader(h->size - size, h, 
-          h->next);
-
-         h->free = 0;
-         h->size = size;
-
-         return ++h;
+   defrag(); 
+   while (curr) {
+      if (curr->free && curr->size >= size) {
+         if (curr->size > size) {
+            curr->next = carveHeader(curr->size - size, curr, curr->next);
+            curr->size = size;
+         }
+         curr->free = 0;
+         return ++curr;
       }
-      else if (h->size < size)
-         sum += h->size;
 
-      if (h->free == 0)
-         sum = 0;
-
-      h = h->next;
+      curr = curr->next;
    }
 
 #if DEBUG_MALLOC
--- test.c
+++ test.c
@@ -14,7 +14,7 @@ void simplePrint(char *s) {
 }
 
 int main(int argc, char **argv) {
-   char *x;
+   char *x, *y, *z;
 
 #if USING_MY_MALLOC
    x = myMalloc(4);
@@ -28,13 +28,90 @@ int main(int argc, char **argv) {
    x = myMalloc(20); 
    simplePrint("allocating 20 bytes...");
    checkFreelist();
+
+   myFree(x);
+   simplePrint("freeing that 20 bytes...");
+   checkFreelist();
+
+   x = myMalloc(21); 
+   simplePrint("allocating 21 bytes...");
+   checkFreelist();
+
+   myFree(x);
+   simplePrint("freeing that 21 bytes...");
+   checkFreelist();
+
+   x = myMalloc(30); 
+   simplePrint("allocating 30 bytes...");
+   checkFreelist();
+
+   myFree(x);
+   simplePrint("freeing that 30 bytes...");
+   checkFreelist();
+   
+   x = myMalloc(100); 
+   simplePrint("allocating 100 bytes...");
+   checkFreelist();
+
+   y = myMalloc(101); 
+   simplePrint("allocating 101 bytes...");
+   checkFreelist();
+
+   myFree(y);
+   simplePrint("freeing that 101 bytes...");
+   checkFreelist();
+
+   y = myMalloc(102); 
+   simplePrint("allocating 102 bytes...");
+   checkFreelist();
+
+   myFree(y);
+   simplePrint("freeing that 102 bytes...");
+   checkFreelist();
+
+   y = myMalloc(200); 
+   simplePrint("allocating 203 bytes...");
+   checkFreelist();
+
+   y = myMalloc(64000);
+   simplePrint("allocating 64K bytes...");
+   checkFreelist();
+
+   myFree(y);
+   simplePrint("freeing that 64K bytes...");
+   checkFreelist();
+
+   y = myMalloc(1024);
+   simplePrint("allocating 1K bytes...");
+   checkFreelist();
+
+   z = myMalloc(64000);
+   simplePrint("allocating 64K bytes...");
+   checkFreelist();
+
+   myFree(z);
+   simplePrint("freeing that 64K bytes...");
+   checkFreelist();
+
+   z = myMalloc(100);
+   simplePrint("allocating 100 bytes...");
+   checkFreelist();
+
+   myFree(x);
+   myFree(y);
+   myFree(z);
+   checkFreelist();
+   myMalloc(64000);
+   checkFreelist();
 #elif USING_REG_MALLOC
    int *x = malloc(16000 * sizeof(int));
 #endif
 
+   /*
    int i;
-   for (i = 0; i < 20; i++) 
-      x[i] = 0 + i;
+   for (i = 0; i < 64000; i++) 
+      z[i] = i % 128;
+   */
 
    return 0;
 }
